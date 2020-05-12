a445da965aa917109bf9e235091aa174b01f937f
Kyle Krupp <kyle.krupp@gmail.com>
2020-05-11 22:38:09 -0400

    update favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
-import React from "react"
-import Helmet from 'react-helmet';
+import React from 'react'
+import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import PostLink from "../components/post-link"
-import HeroHeader from "../components/heroHeader"
+import Layout from '../components/layout'
+import PostLink from '../components/post-link'
+import HeroHeader from '../components/heroHeader'
+
+import favicon from '../images/favicon.ico'
 
 const IndexPage = ({
   data: {
     site,
-    allMarkdownRemark: { edges },
-  },
+    allMarkdownRemark: { edges }
+  }
 }) => {
-
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
@@ -19,15 +20,19 @@ const IndexPage = ({
   return (
     <Layout>
       <Helmet>
+        <link rel='icon' href={favicon} />
         <title>{site.siteMetadata.title}</title>
-        <meta name="description" content={site.siteMetadata.description} />
-        {!site.siteMetadata.w3l_dom_key ? null : <meta name="w3l-domain-verification" content={site.siteMetadata.w3l_dom_key} />}
+        <meta name='description' content={site.siteMetadata.description} />
+        {!site.siteMetadata.w3l_dom_key ? null : (
+          <meta
+            name='w3l-domain-verification'
+            content={site.siteMetadata.w3l_dom_key}
+          />
+        )}
       </Helmet>
-      <HeroHeader/>
+      <HeroHeader />
       <h2>Blog Posts &darr;</h2>
-      <div className="grids">
-        {Posts}
-      </div>
+      <div className='grids'>{Posts}</div>
     </Layout>
   )
 }
